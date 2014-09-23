427eac2f82169434ba481f0ebd458d6df7997d0e
tmeissner <programming@goodcleanfun.de>
2014-09-24 00:08:38 +0200

    example for useful use of property function

---
+++ python_3_oop/chapter05/webpage.py
@@ -0,0 +1,18 @@
+from urllib.request import urlopen
+import time
+
+
+class WebPage:
+
+    def __init__(self, url):
+        self.url = url
+        self._content = None
+
+    @property
+    def content(self):
+        now = time.time()
+        if not self._content:
+            print("Retrieving new page...")
+            self._content = urlopen(self.url).read()
+        print("Got page in {} seconds".format(time.time() - now))
+        return self._content
