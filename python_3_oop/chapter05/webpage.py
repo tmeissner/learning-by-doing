3a6d1e600db14c71855be5e99f81192704391a19
tmeissner <programming@goodcleanfun.de>
2014-10-07 21:44:09 +0200

    added timeout after that the webpage still will be loaded

--- python_3_oop/chapter05/webpage.py
+++ python_3_oop/chapter05/webpage.py
@@ -6,12 +6,14 @@ class WebPage:
 
     def __init__(self, url):
         self.url = url
+        self.an_old_time = 0
         self._content = None
 
     @property
     def content(self):
         now = time.time()
-        if not self._content:
+        if not self._content or (now - self.an_old_time > 10):
+            self.an_old_time = now
             print("Retrieving new page...")
             self._content = urlopen(self.url).read()
         print("Got page in {} seconds".format(time.time() - now))
