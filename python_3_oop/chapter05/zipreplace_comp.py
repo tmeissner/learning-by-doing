47dd9bf1cc5fc7dfffbe8e9c65a3f1e29b85d2a4
tmeissner <programming@goodcleanfun.de>
2014-10-07 22:14:49 +0200

    make it pep8 compliant

--- python_3_oop/chapter05/zipreplace_comp.py
+++ python_3_oop/chapter05/zipreplace_comp.py
@@ -3,7 +3,6 @@ import sys
 import os
 
 
-
 class ZipReplace:
 
     def __init__(self, search_string, replace_string):
@@ -11,12 +10,12 @@ class ZipReplace:
         self.replace_string = replace_string
 
     def process(self, zipprocessor):
-        "perform a search and replace on all files in the temporary directory"
+        """perform a search and replace on all files in the temporary directory"""
         for filename in os.listdir(zipprocessor.temp_directory):
             with open(zipprocessor._full_filename(filename)) as file:
                 contents = file.read()
                 contents = contents.replace(self.search_string,
-                    self.replace_string)
+                                            self.replace_string)
             with open(zipprocessor._full_filename(filename), "w") as file:
                 file.write(contents)
 
