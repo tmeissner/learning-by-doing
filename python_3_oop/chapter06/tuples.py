53bce1fa4d353e09f1522c49ee89e4cf17b9248e
tmeissner <programming@goodcleanfun.de>
2014-10-10 23:48:21 +0200

    completed tupes examples by adding one for named tupes

--- python_3_oop/chapter06/tuples.py
+++ python_3_oop/chapter06/tuples.py
@@ -1,4 +1,5 @@
 import datetime
+from collections import namedtuple
 
 
 def middle(stock, date):
@@ -12,3 +13,10 @@ mid_value, date = middle(tuple, datetime.datetime.now())
 print("middle value: {} at date {}".format(mid_value, date))
 print("high value: {}".format(tuple[2]))
 print(tuple[1:3])
+
+
+# named tuples
+Stock = namedtuple("Stock", "symbol current high low")
+stock = Stock("GOOG", 613.30, high=625.86, low=610.50)
+print("{}: current={}, high={}, low={}".format(stock.symbol, stock.current,
+                                               stock.high, stock.low))
